be8f343d1f75fe3d4a7dad13ddd728394e2b570d
tech-vin <tech.vineetsingh@gmail.com>
2025-09-04 14:00:28 +0530

    added test coverage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DSA-Playbook
 A comprehensive DSA reference with clean code, organized notes, and progress tracking. 
+
+
+## Run the test
+pytest -v
+
+## Run test with coverage
+pytest --cov=src.array_revstring --cov-report=term-missing
+
--- test/test_reverse_string.py
+++ test/test_reverse_string.py
@@ -10,3 +10,43 @@ def test_normal_string():
 
     assert reverseStringUsingTwoPointer('abc') == 'cba'
     assert reverseStringUsingTwoPointer('Let the game begin!') == '!nigeb emag eht teL'
+
+def test_empty_string():
+    assert reverseString('') == ''
+    assert reverseStringUsingBuiltIn('') == ''
+    assert reverseStringUsingTwoPointer('') == ''
+
+def test_valid_input():
+    with pytest.raises(TypeError):
+        reverseString(1234)
+    with pytest.raises(TypeError):
+        reverseString(True)
+    with pytest.raises(TypeError):
+        reverseString(['a', 'b'])
+
+    with pytest.raises(TypeError):
+        reverseStringUsingBuiltIn(1234)
+    with pytest.raises(TypeError):
+        reverseStringUsingBuiltIn(True)
+    with pytest.raises(TypeError):
+        reverseStringUsingBuiltIn(['a', 'b'])
+
+    with pytest.raises(TypeError):
+        reverseStringUsingTwoPointer(1234)
+    with pytest.raises(TypeError):
+        reverseStringUsingTwoPointer(True)
+    with pytest.raises(TypeError):
+        reverseStringUsingTwoPointer(['a', 'b'])
+
+def test_single_char():
+    assert reverseString('V') == 'V'
+    assert reverseStringUsingBuiltIn('V') == 'V'
+    assert reverseStringUsingTwoPointer('V') == 'V'
+
+def test_special_char():
+    assert reverseString('#$@!') == '!@$#'
+    assert reverseStringUsingTwoPointer('#$@!') == '!@$#'
+    assert reverseStringUsingBuiltIn('#$@!') == '!@$#'
+
+
+    
