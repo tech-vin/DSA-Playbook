2c07ece76eaa194c1d478ec7bb3c058f2582c92e
tech-vin <tech.vineetsingh@gmail.com>
2025-09-15 12:58:31 +0530

    added docstring to the twoSum problem

--- problems/leetcode/twoSum.py
+++ problems/leetcode/twoSum.py
@@ -1,6 +1,18 @@
 # https://leetcode.com/problems/two-sum/description/
 
 def twoSum(nums, target):
+    '''
+    Returns indices of 2 numbers whose sum is equal to target
+
+    Example:
+        If the arr is [2, 7, 11, 15] and target is 9, then output will be:
+        [0, 1] because 2 + 7 is 9 and 2 is present at index 0 and 7 at index 1.
+
+    Args:
+        nums [list] : list of array
+        target [int] : integer value
+
+    '''
     nums_dict = {}
     for indx, val in enumerate(nums):
         complement = target - val 
