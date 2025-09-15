17435d49b903ebffe7b2ba26154296042149b5a5
tech-vin <tech.vineetsingh@gmail.com>
2025-09-15 10:12:28 +0530

    leetcode problem "move all zeros" to the end added.

--- problems/leetcode/moveZeros.py
+++ problems/leetcode/moveZeros.py
@@ -1,1 +1,22 @@
 # https://leetcode.com/problems/move-zeroes/
+
+def moveZeros(arr):
+    '''
+    Move all the zeros to the end, while maintaining non-zero number order.
+    
+    Example:
+        if the given arr is [0, 1, 2, 0, 3] then output will be:
+        [1, 2, 3, 0, 0]
+    Args:
+        arr [list] - array of numbers
+
+    '''
+    i = 0
+    for j in range(len(arr)):
+        if arr[j] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            i+=1
+
+arr = [[0, 1, 0, 2], [0, 1, 2], [0], [], [0, -1, -2, 0, -5, 4, 1]]
+[moveZeros(x) for x in arr]
+print([x for x in arr])
